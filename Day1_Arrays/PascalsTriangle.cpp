4afbd1a5ec1dc67ee330f5036c4c5db20734ec71
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2023-06-10 12:19:51 +0530

    Create PascalsTriangle.cpp

---
+++ Day1_Arrays/PascalsTriangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int nCr(int n, int r){
+        long long res = 1;
+        for(int i=0; i<r; i++){
+            res = res * (n-i);
+            res = res/(i+1);
+        }
+        return (int)(res);
+    }
+
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        for(int row=1; row<=n; row++){
+            vector<int> tempLst;
+            for(int col=1; col<=row; col++){
+            tempLst.push_back(nCr(row-1, col-1));
+            }
+            ans.push_back(tempLst);
+        }
+        return ans; 
+    }
+};
