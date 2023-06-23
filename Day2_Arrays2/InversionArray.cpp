54ec36352241a59cd33a3ca5d3fe8341f93211f7
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2023-06-23 14:09:15 +0530

    Create InversionArray.cpp

---
+++ Day2_Arrays2/InversionArray.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    int count = 0;
+    for(int i=0;i<n;i++){
+        for(int j = i+1; j<n; j++){
+            if(arr[i]>arr[j])
+                count++;
+        }
+    }
+    return count;
+}
